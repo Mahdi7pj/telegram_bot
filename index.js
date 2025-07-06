656697468eee73c9b693e74d7ecdadb2acf0f56e
Mahdi7pj <fastcar302@gmail.comm>
2025-07-06 15:35:27 +0330

    Updated bot token to use environment variable

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 
-const bot =new Telegraf('7668968463:AAGQ7Dzhddnrq5ANx1yff6mgKmH_2WIadm4')
+const bot =new Telegraf(process.env.bot_token);
 
 bot.start((ctx) =>{
     ctx.reply('سلام به ربات فروشگاه    خوش آمدید \n چه کمکی از من برمیاد\n /help')
