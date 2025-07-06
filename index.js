7bddd8047af8d99333b791740bc688a05c020ce3
Mahdi7pj <fastcar302@gmail.comm>
2025-07-06 16:03:17 +0330

    help commend

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const { Telegraf } = require('telegraf')
 const bot =new Telegraf(process.env.bot_token);
 
 bot.start((ctx) =>{
-    ctx.reply('سلام به ربات فروشگاه    خوش آمدید \n چه کمکی از من برمیاد\n /help')
+    ctx.reply('سلام به ربات فروشگاه mahdi7pj  خوش آمدید \n چه کمکی از من برمیاد\n /help')
+})
+bot.help((ctx)=>{
+    ctx.reply('این یک ربات فروشگاهی هست.برای ارتباط با ما میتوانید از روش های زیر استفاده کنید .\nآدرس پشیبانی تلگرام :\nآدرس سایت:n\شماره تماس:')
 })
 
 
